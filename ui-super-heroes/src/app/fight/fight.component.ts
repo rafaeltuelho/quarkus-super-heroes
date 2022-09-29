fd8c4bed695075cf969019b28bea15bc15ffbc6c
Rafael T. C. Soares <rafaelcba@gmail.com>
2022-09-29 17:49:07 -0400

    initial experiments with fights UI

--- ui-super-heroes/src/app/fight/fight.component.ts
+++ ui-super-heroes/src/app/fight/fight.component.ts
@@ -1,14 +1,26 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Fighters, FightService, Hero, Villain } from '../shared';
 
 @Component({
   selector: 'hero-fight',
-  templateUrl: './fight.component.html'
+  templateUrl: './fight.component.html',
+  animations: [
+    trigger('fire', [
+      // state(),
+      transition('* => *', [
+        style({border: '5px red solid'}),
+        animate(5)
+      ])
+    ])
+  ]
 })
 export class FightComponent implements OnInit {
 
   figthers: Fighters = new Fighters();
   winner: String;
+  fighting: boolean;
+  firedState: boolean;
 
   constructor(private fightService: FightService) {
   }
@@ -17,7 +29,16 @@ export class FightComponent implements OnInit {
     this.newFighters();
   }
 
+  fire() {
+    console.log('animating!')
+    this.firedState = false;
+    setTimeout(() => {
+      this.firedState = true;
+    }, 1);
+  }
+
   fight() {
+    this.fighting = true;
     this.fightService.apiFightsPost(this.figthers).subscribe(
       fight => {
         this.fightService.onNewFight(fight);
@@ -29,5 +50,7 @@ export class FightComponent implements OnInit {
   newFighters() {
     this.winner = null;
     this.fightService.apiFightsRandomfightersGet().subscribe(figthers => this.figthers = figthers);
+    this.fighting = false;
+    this.firedState = false;
   }
 }
